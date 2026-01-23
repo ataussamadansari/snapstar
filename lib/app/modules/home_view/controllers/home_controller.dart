21b2751458ce0e2e20aaf2d89832ceab6cf4656b
Samad <sam.an.vns6@gmail.com>
2026-01-23 16:00:14 +0530

    update

--- lib/app/modules/home_view/controllers/home_controller.dart
+++ lib/app/modules/home_view/controllers/home_controller.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
+
+
+  @override
+  void onInit() async {
+    super.onInit();
+    String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+    debugPrint("Token: $token");
+  }
+
+
   final stories = List.generate(
     10,
         (index) => 'User $index',
